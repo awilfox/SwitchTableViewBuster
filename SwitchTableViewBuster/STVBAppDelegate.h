427199ce065d38b960822363e42fbf9c9963d02d
Andrew Wilcox <AWilcox@Wilcox-Tech.com>
2012-05-29 21:48:24 -0400

    Initial Commit

---
+++ SwitchTableViewBuster/STVBAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  STVBAppDelegate.h
+//  SwitchTableViewBuster
+//
+//  Created by Andrew Wilcox on 29/05/2012.
+//  Copyright (c) 2012 Wilcox Technologies. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface STVBAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SwitchTableViewBuster/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SwitchTableViewBuster
+//
+//  Created by Andrew Wilcox on 29/05/2012.
+//  Copyright (c) 2012 Wilcox Technologies. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "STVBAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([STVBAppDelegate class]));
+	}
+}
